19d631a4bed0dcc2bf92668e554ce0fd8e541a25
neec <neec@localhost.localdomain>
2018-02-05 16:21:40 +0900

    変更

--- kataoka/Join.php
+++ kataoka/Join.php
@@ -12,7 +12,7 @@ $pw   = 'H@chiouji1';   //MySQLのパスワード
 // ユーザー名処理
 //-------------------------------------------------
 	//同じ名前がいるかどうか
-	$isName=FALSE;
+	$isName='FALSE';
 	
 	$dbh = new PDO($dsn, $user, $pw);   //接続
 	//テーブルの中身を表示
@@ -20,10 +20,10 @@ $pw   = 'H@chiouji1';   //MySQLのパスワード
 	$result = $dbh -> query($sql);
 	//同じ名前がいるかどうか
 	foreach($result as $row){
-		if($row['name']==$name) $isName=TRUE;
+		if($row['name']==$name) $isName='TRUE';
 	}
 	//いなかったらsqlに登録
-	if($isName==FALSE){
+	if($isName=='FALSE'){
 	$sql = 'insert into JunkenState values(:name,-1)';//新しいデータを入れる
 	$sth = $dbh->prepare($sql);         //SQL準備
 	$sth->bindValue(':name', $name, PDO::PARAM_STR);
